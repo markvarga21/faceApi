55c09079e460ec75a304ea2ceb56021d36d9d985
Varga József Márk <vmark2145@gmail.com>
2023-08-18 18:21:24 +0300

    Added Dockerfile and modified requirements.txt.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request
+#from flask_cors import CORS, cross_origin
 from face_service import validate_faces
 
 app = Flask(__name__)
+#cors = CORS(app)
 
+#@cross_origin()
 @app.route("/")
 def index():
     return "Hi! Please visit the Github Repo (https://github.com/markvarga21/faceApi) for usage!"
 
+#@cross_origin()
 @app.route("/validate", methods=['GET'])
 def validateImages():
     idPhoto = request.files['idPhoto']
@@ -14,3 +18,6 @@ def validateImages():
     output = validate_faces(idPhoto, selfiePhoto)
 
     return output
+
+if __name__ == '__main__':
+    app.run(debug=True)
