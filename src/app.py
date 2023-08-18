80c045ce73bbda2e75fa57346a3fa9a7d2df074a
Varga József Márk <vmark2145@gmail.com>
2023-08-18 16:28:57 +0300

    Finished face validator.

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from face_service import validate_faces
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@cross_origin()
+@app.route("/validate", methods=['GET'])
+def hello_world():
+    idPhoto = request.files['idPhoto']
+    selfiePhoto = request.files['selfiePhoto']
+    output = validate_faces(idPhoto, selfiePhoto)
+
+    return output
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
